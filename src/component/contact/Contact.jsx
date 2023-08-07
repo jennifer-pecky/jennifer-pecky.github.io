583ddcc9bea64bdced81affc5ce35aac607a2b84
jennifer-pecky <ugwojennifer7@gmail.com>
2023-08-07 16:22:00 +0100

    update

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,64 +1,90 @@
-import React from 'react'
-import './contact.css'
-import { HiMail } from 'react-icons/hi'
-import { BsTwitter } from 'react-icons/bs'
-import { BsWhatsapp } from 'react-icons/bs'
+import React from 'react';
+import './contact.css';
+import { HiMail } from 'react-icons/hi';
+import { BsTwitter } from 'react-icons/bs';
+import { FiLinkedin } from 'react-icons/fi';
 import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
 
-    const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    emailjs.sendForm(
+      'service_uh4k4gm',
+      'template_ns1pajd',
+      form.current,
+      'upYp6yJ4Wee5EDLvd'
+    );
 
-        emailjs.sendForm('service_uh4k4gm', 'template_ns1pajd', form.current, 'upYp6yJ4Wee5EDLvd')
+    e.target.reset;
+  };
 
-        e.target.reset
-    };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
 
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ugwojennifer7@gmail.com</h5>
+            <a href="mailto:ugwojennifer7@gmail.com" target="_blank">
+              Send a Msessage
+            </a>
+          </article>
 
+          <article className="contact__option">
+            <BsTwitter className="contact__option-icon" />
+            <h4>Twitter</h4>
+            <h5>My Twitterhandle</h5>
+            <a href="https://twitter.com/Pecky___" target="_blank">
+              Send a Msessage
+            </a>
+          </article>
 
+          <article className="contact__option">
+            <FiLinkedin className="contact__option-icon" />
+            <h4>linkedin</h4>
+            <h5>Lets connect</h5>
+            <a
+              href="https://www.linkedin.com/in/jennifer-chioma-maduagwu-7051ab236/"
+              target="_blank"
+            >
+              Send a Msessage
+            </a>
+          </article>
+        </div>
+        <form ref={form}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea
+            message=""
+            id=""
+            rows="7"
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button
+            type="submit"
+            onSubmit={sendEmail}
+            className="btn btn-primary"
+          >
+            Send messaage
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section id='contact'>
-            <h5>Get In Touch</h5>
-            <h2>Contact Me</h2>
-
-            <div className='container contact__container'>
-                <div className='contact__options'>
-                    <article className='contact__option'>
-                        <HiMail className='contact__option-icon' />
-                        <h4>Email</h4>
-                        <h5>ugwojennifer7@gmail.com</h5>
-                        <a href="mailto:ugwojennifer7@gmail.com" target="_blank">Send a Msessage</a>
-                    </article>
-
-                    <article className='contact__option'>
-                        <BsTwitter className='contact__option-icon' />
-                        <h4>Twitter</h4>
-                        <h5>My Twitterhandle</h5>
-                        <a href="https://twitter.com/Pecky___" target="_blank" >Send a Msessage</a>
-                    </article>
-
-                    <article className='contact__option'>
-                        <BsWhatsapp className='contact__option-icon' />
-                        <h4>Whatsapp</h4>
-                        <h5>+12345678</h5>
-                        <a href="http://wa.link/crxybr" target="_blank" >Send a Msessage</a>
-                    </article>
-
-                </div>
-                <form ref={form}>
-                    <input type="text" name='name' placeholder='Your full name' required />
-                    <input type="email" name='email' placeholder='Your email' required />
-                    <textarea message="" id="" rows="7" placeholder='Your message' required></textarea>
-                    <button type='submit' onSubmit={sendEmail} className='btn btn-primary'>Send messaage</button>
-                </form>
-            </div>
-        </section>
-    )
-}
-
-export default Contact
+export default Contact;
