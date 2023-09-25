394bb25a121d1192f2fab393c619c7146c64ad55
jennifer-pecky <ugwojennifer7@gmail.com>
2023-09-25 13:09:42 +0100

    added personal social links

--- src/component/header/HeaderSocials.jsx
+++ src/component/header/HeaderSocials.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
-import { FaTwitter } from 'react-icons/fa'
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { FaTwitter } from 'react-icons/fa';
 
 const HeaderSocials = () => {
-    return (
-        <div className="header__socials">
-            <a href="http://linkedin.com" target="_blank" id='line'><BsLinkedin /></a>
-            <a href="http://facebook.com" target="_blank" id='line'><BsGithub /></a>
-            <a href="http://twitter.com" target="_blank" id='line'><FaTwitter /></a>
-        </div>
-    )
-}
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/jennifer-chioma-maduagwu-7051ab236/"
+        target="_blank"
+        id="line"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://web.facebook.com/jennifer.chioma.7771/"
+        target="_blank"
+        id="line"
+      >
+        <BsGithub />
+      </a>
+      <a href="https://twitter.com/pecky_jennifer" target="_blank" id="line">
+        <FaTwitter />
+      </a>
+    </div>
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
